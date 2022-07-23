785aecbae26f899fc36f1306c103bba9bdd4e671
Francis Aguinaldo <fryaguinaldo@gmail.com>
2022-07-22 23:35:04 -0700

    add updates:

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,7 +1,20 @@
-
 export function validateEmail(email) {
 
     var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
+
+}
+
+export function validateMessage(message) {
+
+    const mess = /^[A-za-z0-9]&/;
+    return mess.test(String(message));
+
+}
+
+export function validateName(inputName) {
+
+    const inName = /^[A-za-z0-9]{2,40}$/;
+    return inName.test(String(inputName));
 
 }
