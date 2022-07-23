7e53c4dbed18db72b216d290ead5ae2def7245e3
Francis Aguinaldo <fryaguinaldo@gmail.com>
2022-07-23 12:43:30 -0700

    add updates

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -6,24 +6,24 @@ function Navigation({ currentPage, handlePageChange }) {
             <ul>
 
                 <li>
-                    <a href="#about" onClick={() => handlePageChange("About")} className={currentPage === "About"}>
+                    <a href="#about" onClick={() => handlePageChange("About")} className={currentPage === "About" ? 'nav-link active' : 'nav-link'}>
                         About
                     </a>
                 </li>
 
                 <li>
-                    <a href="#work" onClick={() => handlePageChange("Projects")} className={currentPage === "Projects"}>
+                    <a href="#work" onClick={() => handlePageChange("Projects")} className={currentPage === "Projects" ? 'nav-link active' : 'nav-link'}>
                         Work
                     </a>
                 </li>
 
                 <li>
-                    <a href="#contact" onClick={() => handlePageChange("Contact")} className={currentPage === "Contact"}>
+                    <a href="#contact" onClick={() => handlePageChange("Contact")} className={currentPage === "Contact" ? 'nav-link active' : 'nav-link'}>
                         Contact
                     </a>
                 </li>
                 <li>
-                    <a href="#resume" onClick={() => handlePageChange("Resume")} className={currentPage === "Resume"}>
+                    <a href="#resume" onClick={() => handlePageChange("Resume")} className={currentPage === "Resume" ? 'nav-link active' : 'nav-link'}>
                         Resume
                     </a>
                 </li>
