749132b5e91bc1d38472f021ac4f724943aac17c
Francis Aguinaldo <fryaguinaldo@gmail.com>
2022-07-22 22:28:38 -0700

    updated index files for contact and footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <div class="footer" id="footer">
+            <a href="https://github.com/nayrsicnarf"><img src="https://img.icons8.com/ios-glyphs/344/github.png" alt="Github" class="icon" /></a>
+            <a href="https://www.linkedin.com/in/fryaguinaldo/"><img src="https://img.icons8.com/ios-glyphs/344/linkedin-circled--v1.png" alt="LinkedIn" class="icon" /></a>
+            <a href="https://drive.google.com/file/d/1O5mleNTwKkEs69FJrzQTggQ9LPRMw2iI/view?usp=sharing"><img src="https://img.icons8.com/ios-glyphs/344/resume.png" alt="Resume" class="icon" /></a>
+            <p>
+                &copy; Copyright 2022 Francis Ryan Aguinaldo
+            </p>
+
+        </div>
+    )
+}
+
+export default Footer;
